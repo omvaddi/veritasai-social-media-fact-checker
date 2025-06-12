b1c14edaee3a2dddafe870f5d667dd8c72ddc706
Om Vaddi <om.r.vaddi@gmail.com>
2025-06-12 13:27:59 -0400

    cleaned up codebase and reduced repeated similar claims

--- backend/FactCheck.py
+++ backend/FactCheck.py
@@ -2,7 +2,6 @@ import yt_dlp
 import tempfile
 import os
 from openai import OpenAI
-from transformers import pipeline
 from fastcoref import spacy_component
 import spacy
 from sentence_transformers import SentenceTransformer, util
@@ -16,29 +15,24 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from urllib.parse import urlparse
 import shutil
-from bertopic import BERTopic
-import umap
-
-
 
 
 load_dotenv()
-
+tmpdir = tempfile.mkdtemp()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
-
-tmpdir = tempfile.mkdtemp()
 nlp = spacy.load("en_core_web_sm")
 model = SentenceTransformer('all-MiniLM-L6-v2')
-
-cache = {}
 
 nlp.add_pipe(
     "fastcoref", 
     config={'model_architecture': 'LingMessCoref', 'model_path': 'biu-nlp/lingmess-coref', 'device': 'cpu'}
 )
+
+cache = {}
+
 
 def download_audio(url: str) -> str:
     ydl_opts = {
@@ -58,7 +52,6 @@ def download_audio(url: str) -> str:
 
 
 def transcribe(path: str) -> str:
-
     with open(path, "rb") as audio_file:
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
@@ -68,6 +61,7 @@ def transcribe(path: str) -> str:
         
         return transcription.text
     
+
 def coref(transcription: str) -> str:
     doc = nlp(
         transcription,
@@ -75,6 +69,7 @@ def coref(transcription: str) -> str:
     )
 
     return(doc._.resolved_text)
+
 
 def splitSentences(coref: str) -> str:
     doc = nlp(coref)
@@ -84,45 +79,26 @@ def splitSentences(coref: str) -> str:
 
     
 def cluster(sentences: list) -> list:
-    
     if len(sentences) == 1:
         return [sentences]
 
     embeddings = model.encode(sentences)
     embeddings = normalize(embeddings)
 
-
-    if len(sentences) < 10:
-        hdbscan_model = hdbscan.HDBSCAN(min_cluster_size=2, metric = 'euclidean')
-        labels = hdbscan_model.fit_predict(embeddings)
-    else:
-        hdbscan_model = hdbscan.HDBSCAN(min_cluster_size=2, min_samples=1, prediction_data=True)
-        umap_model = umap.UMAP(n_neighbors=5, n_components=2, min_dist=0.0, metric='cosine')
-        topic_model = BERTopic(hdbscan_model=hdbscan_model, umap_model=umap_model, calculate_probabilities=True)
+    hdbscan_model = hdbscan.HDBSCAN(min_cluster_size=2, metric = 'euclidean')
+    labels = hdbscan_model.fit_predict(embeddings)
     
-        labels, _ = topic_model.fit_transform(sentences)
-    
-
     clusters = {}
-
     for claim, label in zip(sentences, labels):
         clusters.setdefault(label, []).append(claim)
-
     result = list(clusters.values())
-    print(result)
-
-    print('\n\n\n')
 
     return result
 
 
 def detectClaims(clusters: list) -> dict:
-
-
     groupClusters = [""] * len(clusters)
-
     text = ""
-
     for i in range(len(clusters)):
         for j in range(len(clusters[i])):
             text = text + clusters[i][j] + " "
@@ -133,7 +109,7 @@ def detectClaims(clusters: list) -> dict:
     system_prompt = """
     You are helping extract verifiable claims from social media videos.
     I will give you the full transcipt for contextual purposes but just work with the sentences I want you to analyze.
-    These sentences may not be coherent together but they all follow a theme.
+    These sentences may not be coherent together but they all follow a theme. I will also give you previously used claims; do not create any claims that are almost identical.
 
     Instructions:
     1. Identify a **specific main theme** of the sentence group.
@@ -141,7 +117,7 @@ def detectClaims(clusters: list) -> dict:
     3. Ignore: 
         - Subjective opinions (e.g., what someone feels, prefers, or thinks is fair)
         - Vague or anecdotal statements including statements without context
-        - Private experiences (unless made by a public figure)
+        - The speaker's private experiences when the speaker is unknown
         - Claims relying solely on visuals or unclear references
     4. Each claim must be **specific, concise**, and **not just a rewording of another**.
     5. If a claim is exxagerated or speculative, rephrase it into a neutral, fact-checkable version.
@@ -168,29 +144,34 @@ def detectClaims(clusters: list) -> dict:
     The full transcript (to be used for context only) is as follow: %s
 
     The sentence(s) to analyze are: %s 
+
+    The already used claims are: %s
     """
 
     JSON = {}
+    usedClaims = ""
 
     for i in range(len(groupClusters)):
-
         response = client.chat.completions.create(
             model="gpt-4.1",
             messages = [
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt  % (text, groupClusters[i])}
+                {"role": "user", "content": user_prompt  % (text, groupClusters[i], usedClaims)}
             ],
             response_format = {"type" : "json_object"}
         )
 
         result = response.choices[0].message.content
-
         if result == "SKIP":
             continue
-
         resJSON = json.loads(result)
+        for claim in resJSON["claims"]:
+            usedClaims += " " + claim["text"]
 
+        print(usedClaims)
+        
         JSON["Topic #" + str(i + 1)] = resJSON
+        
 
     return JSON
 
@@ -198,9 +179,7 @@ def detectClaims(clusters: list) -> dict:
 def searchAndAnalyzeEvidence(JSON: dict) -> dict:
     googlecloud_api_key = os.getenv("GOOGLECLOUD_API_KEY")
     search_engine_id = os.getenv("SEARCH_ENGINE_ID")
-
     url = 'https://www.googleapis.com/customsearch/v1'
-
 
     system_prompt = """
     You are an AI fact-checker. You will recieve:
@@ -211,7 +190,6 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
     1. Determine whether the **claim is supported or contradicted** by the snippets.
     2. Base your judgement **primarily on the snippets** but you may make **basic, explicit inferences of simple calculations** when logically justified by the data.
     3. Look for **consistent patterns** across multiple sources.
-    4. If the snippets do not supply clear or relevent evidence, return **"Insufficient Evidence"**
 
     Return the result in this JSON format:
     {
@@ -219,13 +197,13 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
                    | "Likely True" // mostly supported, minor uncertainty
                    | "Likely False" // most contradicted, minor nuance
                    | "False" // clearly disproven
-                   | "Insufficent Evidence" // not enough evidence or too mixed
+                   | "Insufficient Evidence" // not enough evidence or too mixed
                    ,
-        "explanation": "A brief explanation of your reasoning. Try to use quotes and refer to sources by their name mentioning them. Do not mention the fact you are reading from sources/snippets/searches just say things as facts. Additionally if there is no relevent evidence simply say that.",
+        "explanation": "A brief explanation of your reasoning. Try to use quotes and refer to sources by their name. Do not mention the fact you are reading from sources/snippets/searches just say things as facts. Additionally if there is no relevent evidence simply say that.",
         "links": [
             "https://example.com/article1",
             ...
-            // up to three links that best illustrate your verdict
+            // up to three links that best illustrate your verdict. Try to add links even if there is insufficient evidence.
         ]
     }
     """
@@ -237,30 +215,25 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
     """
 
     topic_keys = list(JSON.keys())
-
     for topic_key in topic_keys:
         if not JSON[topic_key]["claims"]:
             del JSON[topic_key]
             continue
 
-
         updatedClaims = []
 
-
         for claim in JSON[topic_key]["claims"]:
-
             search_query = claim["query"]
-
             params = {
                 'q': search_query,
                 'key': googlecloud_api_key,
                 'cx': search_engine_id
             }
 
-            evidence = []
-
             response = requests.get(url, params=params)
             results = response.json().get("items", [])
+            
+            evidence = []
             for i in range(len(results)):
                 piece = {}
                 piece["Article #"] = i + 1
@@ -268,7 +241,6 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
                 piece["link"] = results[i].get("link")
                 piece["snippet"] = results[i].get("snippet")
                 evidence.append(piece)
-
             evStr = json.dumps(evidence, indent = 2)
 
             response = client.chat.completions.create(
@@ -281,7 +253,6 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
             )
 
             evInterpJSON = response.choices[0].message.content
-
             evInterp = json.loads(evInterpJSON)
 
             updatedClaim = {
@@ -292,7 +263,6 @@ def searchAndAnalyzeEvidence(JSON: dict) -> dict:
                 "explanation": evInterp["explanation"],
                 "links": evInterp["links"]
             }
-
             updatedClaims.append(updatedClaim)
 
         JSON[topic_key]["claims"] = updatedClaims
@@ -314,10 +284,12 @@ def factChecker(link: str) -> dict:
         print(json.dumps(verdict, indent = 4))
         shutil.rmtree(tmpdir)
         cache[link] = verdict
+
     return verdict
 
 def is_valid_url(link):
     result = urlparse(link)
+
     if not all([result.scheme in ('http', 'https'), result.netloc]):
         return False
     
@@ -328,6 +300,7 @@ def is_valid_url(link):
     except Exception as e:
         return False
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -335,13 +308,17 @@ CORS(app)
 def fact_check():
     data = request.get_json()
     link = data.get("video_url")
+
     if not link or not is_valid_url(link):
         print(link)
         print(is_valid_url(link))
         return jsonify({"error": "Invalid or missing video URL. Please try again."}), 400
+    
     result = factChecker(link)
+
     if not result:
         return jsonify({"message": "No verifiable claims found."})
+    
     return jsonify(result)
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,65 +8,14 @@ function FactCheck() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null)
   const [message, setMessage] = useState(null)
-
-  const dummyResult = {
-    "Topic #1": {
-        "theme": "Definitions of womanhood and gender participation in sports",
-        "claims": [
-            {
-                "id": 1,
-                "text": "A woman is somebody that can have a baby under certain circumstances.",
-                "query": "Can all women have babies under certain circumstances?",
-                "verdict": "Unclear",
-                "explanation": "There is no relevant evidence provided in these search results directly addressing or defining what a 'woman' is, nor whether the ability to have a baby is central to that definition. The snippets refer to topics such as breastfeeding, Medicaid benefits for pregnant women, and women's health rights, but none provide clear or direct support or contradiction for the claim that a woman is defined as somebody who can have a baby under certain circumstances.",
-                "links": [
-                    "https://www.cdc.gov/breastfeeding-special-circumstances/hcp/exposures/lead.html",
-                    "https://www.pa.gov/services/dhs/apply-for-medicaid-benefits.html",
-                    "https://www.dol.gov/general/topic/health-plans/cobra"
-                ]
-            },
-            {
-                "id": 2,
-                "text": "Men are able to play in women's sports in some cases.",
-                "query": "Are men able to compete in women's sports in some situations?",
-                "verdict": "Likely True",
-                "explanation": "There is evidence that, in certain cases, men (specifically transgender women, who were assigned male at birth but identify and compete as women) have been allowed to compete in women's sports. The snippet from 'Keeping Men Out of Women's Sports \u2013 The White House' directly states that 'athletic associations have allowed men to compete in women's sports.' The SF.gov article gives historical examples, citing 'many examples over the last several decades of transwomen competing in sports,' and names Renee Richards, a trans woman who competed in women's tennis. However, some of the most recent policies, such as the NCAA policy described, are making such participation more restrictive. Overall, there is clear evidence that in some circumstances, men (including trans women) have been able to participate in women's sports.",
-                "links": [
-                    "https://www.whitehouse.gov/presidential-actions/2025/02/keeping-men-out-of-womens-sports/",
-                    "https://www.sf.gov/trans-women-in-sports-facts-over-fear",
-                    "https://www.espn.com/espn/story/_/id/38209262/transgender-athlete-laws-state-legislation-science"
-                ]
-            }
-        ]
-    }
-}
-
-  const useDummy = false;
-  const dummyLoading = false;
-
   
-  
-
   const handleSubmit = async (e) => {
-    
     setResult(null);
     setError(null);
     setMessage(null)
     setLoading(true)
 
-
     e.preventDefault()
-    if (useDummy){
-      setResult(dummyResult);
-      setLoading(false)
-      return;
-    }
-    if (dummyLoading){
-      setLoading(true)
-      return;
-    }
-
-  
 
     const response = await fetch("http://localhost:5000/", {
       method: "POST",
@@ -78,7 +27,6 @@ function FactCheck() {
 
     const json = await response.json();
 
-    
     if(!response.ok){
       setError(json.error || "Unknown error occured");
       setLoading(false)
@@ -91,7 +39,6 @@ function FactCheck() {
       return;
     }
 
-
     setResult(json)
     setLoading(false)
   }
@@ -99,10 +46,9 @@ function FactCheck() {
   return (
     <div style = {{ padding: "2rem", fontFamily: 'Lexend'}} >
       <h1>Veritas</h1>
-    
+      
       <div style={{ display: "flex", justifyContent: "center", marginTop: "2rem"}}>
         <form onSubmit={handleSubmit} className="search">
-      
           <input
             className="search-input"
             type="text"
@@ -112,8 +58,8 @@ function FactCheck() {
           />
         </form>
       </div>
+
       {loading && (
-  
         <div className="loading-container">
           <p>Analyzing video, please wait...</p>
           <div className="loading"></div>
@@ -122,7 +68,6 @@ function FactCheck() {
           <div className="loading"></div>
           <div className="loading"></div>
         </div>
-    
       )}
 
       {error && (
